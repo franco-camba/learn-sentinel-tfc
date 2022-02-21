12635703ce1c498bff43f6cea1e4181d18d94e2f
franco-camba <franco.camba@hashicorp.com>
2022-02-21 20:50:01 +0000

    pushing changes

--- main.tf
+++ main.tf
@@ -30,7 +30,8 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "ubuntu" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
+  //instance_type          = var.instance_type
+  instance_type = "t2.2xlarge"
 
   tags = {
     Name = var.instance_name
